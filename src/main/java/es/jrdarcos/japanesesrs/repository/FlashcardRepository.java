16ce75f7aeb538316ceaa1e81162774a8d78e2ef
joaquin <jruiz@properly.cloud>
2026-04-27 20:28:25 +0200

    fix: add @Repository to both UserRepository and FlashcardRepository

--- src/main/java/es/jrdarcos/japanesesrs/repository/FlashcardRepository.java
+++ src/main/java/es/jrdarcos/japanesesrs/repository/FlashcardRepository.java
@@ -2,10 +2,12 @@ package es.jrdarcos.japanesesrs.repository;
 
 import es.jrdarcos.japanesesrs.entity.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
     List<Flashcard> findByNextReviewLessThanEqual(LocalDate date);
 }
--- src/main/java/es/jrdarcos/japanesesrs/repository/UserRepository.java
+++ src/main/java/es/jrdarcos/japanesesrs/repository/UserRepository.java
@@ -2,6 +2,8 @@ package es.jrdarcos.japanesesrs.repository;
 
 import es.jrdarcos.japanesesrs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
